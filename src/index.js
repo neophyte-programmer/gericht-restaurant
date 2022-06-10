1ed79fd20619213e497140f4a407c185eeb5fb2b
Nutifafa Attor <attorfafa@gmail.com>
2022-06-10 12:04:22 +0000

    switched to react 18

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 
