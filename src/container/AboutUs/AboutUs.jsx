6edd513cc5d7746f09c50951c3326ef29470f48b
Nutifafa Attor <attorfafa@gmail.com>
2022-06-10 23:01:23 +0000

    about section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React from 'react'
 
-import './AboutUs.css';
+import { images } from '../../constants'
+
+import './AboutUs.css'
 
 const AboutUs = () => (
-  <div>
-    AboutUs
-  </div>
-);
+	<div
+		className='app__aboutus app__bg flex__center section__padding'
+		id='about'
+	>
+		<div className='app__aboutus-overlay flex__center'>
+			<img src={images.G} alt='g letter' />
+		</div>
 
-export default AboutUs;
+    <div className='app__aboutus-content flex__center'>
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="spoon" className='spoon__img' />
+        <p className="p__opensans">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.
+        </p>
+        <button type='button' className="custom__button">Learn More</button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about img" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="spoon" className='spoon__img' />
+        <p className="p__opensans">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.
+        </p>
+        <button type='button' className="custom__button">Learn More</button>
+      </div>
+
+    </div>
+	</div>
+)
+
+export default AboutUs
