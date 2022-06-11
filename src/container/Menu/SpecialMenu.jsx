b55e34ce5447c21674bbd50236a6541aed56658d
Nutifafa Attor <attorfafa@gmail.com>
2022-06-11 09:01:08 +0000

    minor change

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { SubHeading, MenuItem } from '../../components'
+import { images, data } from '../../constants'
 import './SpecialMenu.css'
 
-import { images, data } from '../../constants'
 
 const SpecialMenu = () => (
 	<div className='app__specialMenu flex__center section__padding' id='menu'>
