570196c7126cdbec1f811907aa6cc816e6d22a03
Nutifafa Attor <attorfafa@gmail.com>
2022-06-11 12:46:09 +0000

    footer overlay

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 import './FooterOverlay.css';
 
 const FooterOverlay = () => (
-  <div>
-    FooterOverlay
+  <div className='app__footerOverlay'>
+    <div className="app__footerOverlay-black" />
+    <div className="app__footerOverlay-img app__bg" />
+
   </div>
 );
 
