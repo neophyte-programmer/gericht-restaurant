bbbeeee1d56aa0543974aafaa2cc8d02e06119c7
Nutifafa Attor <attorfafa@gmail.com>
2022-06-11 12:46:02 +0000

    update

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import { FooterOverlay, Newsletter } from '../../components';
+import { FiFacebook, FiInstagram, FiTwitter } from 'react-icons/fi';
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className='app__footer section__padding'>
+    <FooterOverlay />
+    <Newsletter />
   </div>
 );
 
