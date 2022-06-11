a6116fc892afbc54d506c1e7b5d72afe80009f1b
Nutifafa Attor <attorfafa@gmail.com>
2022-06-11 12:45:53 +0000

    newsletter section

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import SubHeading  from '../SubHeading/SubHeading';
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className="app__newsletter-heading">
+      <SubHeading title='Newsletter' />
+      <h1 className="headtext__cormorant">Subscribe to Our Newsletter</h1>
+      <p className="p__opensans">
+        Never miss latest updates!
+      </p>
+    </div>
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder="Enter your email" />
+      <button className="custom__button">Subscribe</button>
+    </div>
   </div>
 );
 
